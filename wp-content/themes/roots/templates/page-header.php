860b78da1cf911746cef03fb978b0445e6381148
Eric Tendian <erictendian@gmail.com>
2013-08-04 10:05:45 +0000

    Fixed breadcrumb link

--- wp-content/themes/roots/templates/page-header.php
+++ wp-content/themes/roots/templates/page-header.php
@@ -3,7 +3,7 @@
 	<div class="container">
         <h1 class="pull-left"><?php echo roots_title(); ?></h1>
         <ul class="pull-right breadcrumb">
-            <li><a href="index.html">Home</a> <span class="divider">/</span></li>
+            <li><a href="<?php echo home_url(); ?>/">Home</a> <span class="divider">/</span></li>
             <li class="active"><?php echo roots_title(); ?></li>
         </ul>
     </div><!--/container-->
