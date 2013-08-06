a102638941af4d1360bd814b41b8adac97472667
Eric Tendian <erictendian@gmail.com>
2013-08-06 00:52:21 +0000

    Fixed again?

--- wp-content/themes/roots/templates/comment.php
+++ wp-content/themes/roots/templates/comment.php
@@ -1,9 +1,10 @@
 <?php echo get_avatar($comment, $size = '64'); ?>
 <div class="media-body">
-  <h4 class="media-heading"><?php echo get_comment_author_link(); ?> <span>
-  <time datetime="<?php echo comment_date('c'); ?>"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s', 'roots'), get_comment_date(),  get_comment_time()); ?></a></time>
-   <?php comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth'], 'before' => '/ '))); ?>
-   <?php edit_comment_link(__('(Edit)', 'roots'), '/ ', ''); ?></span></h4>
+  <h4 class="media-heading"><?php echo get_comment_author_link(); ?>
+    <span>
+      <time datetime="<?php echo comment_date('c'); ?>"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s', 'roots'), get_comment_date(),  get_comment_time()); ?></a></time><?php comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth'], 'before' => '/ '))); ?><?php edit_comment_link(__('Edit', 'roots'), '/ ', ''); ?>
+    </span>
+  </h4>
 
   <?php if ($comment->comment_approved == '0') : ?>
     <div class="alert">
