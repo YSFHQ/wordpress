25a6b97e7404ba386d3e5f62580d6b530640675f
Eric Tendian <erictendian@gmail.com>
2014-01-08 20:59:54 +0000

    Updates

--- wp-content/themes/roots/lib/custom.php
+++ wp-content/themes/roots/lib/custom.php
@@ -32,7 +32,7 @@ function of_get_option($name, $default = false) {
 
 if ( !function_exists( 'is_first_class' ) ) {
     function is_first_class() {
-        $exceptions = array("/community/online-servers", "/login");
+        $exceptions = array("/community/online-servers", "/login", "/2013-in-review");
         foreach ($exceptions as $except) if (strpos($_SERVER["REQUEST_URI"], $except)!==false) return true;
         return array_key_exists(11, pp_get_groups_for_user(get_current_user_id(),'pp_group',array("cols"=>"id","status"=>"active")));
     }
