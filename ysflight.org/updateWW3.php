483d7b84fd03fac054b5454a7d0134df77d76f88
Eric Tendian <erictendian@gmail.com>
2014-01-25 03:54:16 -0500

    Updated WW3 script

---
+++ ysflight.org/updateWW3.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+  file_put_contents('../../forum.ysfhq.com/static_message.html', sprintf('There are currently <strong>%d</strong> players on the <a href="ysflight://ys.ysfhq.com">World War 3 server</a>. | <a href="http://ysfhq.com/ww3/">More info</a>', intval($_GET['num'] ? $_GET['num'] : 0)));
+  echo "ww3 player count updated";
+} catch (Exception $e) {
+  echo "cannot update ww3 player count";
+}
+
+?>
